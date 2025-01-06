0611937dcd3cac125a400e1501aad3988cebc3f0
PhanPhouth <phanphouth68@gmail.com>
2025-01-06 21:46:01 +0700

    add

---
+++ Lesson1/exercise1.js
@@ -0,0 +1,12 @@
+alert('Good morning!');
+alert('Nicolas Jackson');
+10 + 5
+15
+20 - 5
+15
+2 + 2 - 5
+-1
+document.body.innerHTML = 'Good morning!';
+'Good morning!'
+document.body.innerHTML = 'Nicolas Jackson';
+'Nicolas Jackson'
---
+++ lesson2/exercise.js
@@ -0,0 +1,52 @@
+// Basics of Math:
+2 + 2
+10 - 3
+10 * 3
+10 / 2
+
+2 + 2 + 2
+2.2 + 2.2
+
+10.90 * 2 + 20.95
+10.90 * 2 + 20.95 + 4.99
+
+// Order of operations:
+1 + 1 * 3
+(1 + 1) * 3
+
+20.95 + 7.99 * 2
+36.93 * 0.1
+(20.95 + 7.99 * 2) * 0.1
+
+// Inaccuracies with Floats:
+0.1 + 0.2
+20.95 + 7.99
+(2095 + 799) / 100
+
+// How to round numbers:
+Math.round(2.2)
+Math.round(2.8)
+
+((2095 + 799) * 0.1) / 100
+Math.round((2095 + 799) * 0.1) / 100
+((2095 + 799 + 1899 ) * 0.1 )/ 100
+4.793
+
+Math.round((2095 + 799 + 1899 + 499 ) * 0.1)/ 100
+5.29
+(Math.round((2095 + 799 + 1899 + 499 ) * 0.1) + (2095 + 799 + 1899 + 499))/ 100
+58.21
+// down 
+Math.floor(2.8)
+2
+//up
+Math.ceil(2.2)
+3
+let fahrenheit = (25 * 9 / 5) + 32
+alert(fahrenheit)
+let celsius = ( 86 - 32 ) * 5 / 9
+alert(celsius)
+let fahrenheit = (-5 * 9 / 5) + 32
+alert(FileReader)
+//can use nagative number
+console.log(alert(fahrenheit))
